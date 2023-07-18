1b8935420f918f8c1e245c8a2ac8d62f0beb649e
heidikrisse <heidi.kristiina90@gmail.com>
2023-07-18 17:11:20 +0300

    Added Assignment 6.2.3
    Assignment:
    
    We are going to make our first improvements upon our ex 6.1.2 class.
    (I'll call it Object for brevity from here on)
    
    Create constructor overloads to the class to make conversions from integer, string / char and
    floating point value into our Object type.  After this e.g. this should work:
    
    Object a(42);  // stores integer, sets the state accordingly
    Object b("BlahBlah"); // stores initial (char), sets the state
    Object c(5.28); // stores double...
    
    Think about do we need a destructor here to clean up after ourselves, and why / why not?
    
    (optional) Try adding explicit specifier the constructors and check how the usage of the
    constructors differs afterwards.  Think about when you would want to use it and when not.

---
+++ week6/Assignment_6.2.3/include/tagged_union.h
@@ -0,0 +1,47 @@
+// tagged_union.h
+// Exercise 1
+#ifndef TAGGED_UNION_H
+#define TAGGED_UNION_H
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+enum class TaggedUnionType
+{
+    person,
+    robot,
+    alien,
+};
+
+class TaggedUnion
+{
+public:
+    TaggedUnionType type;
+
+    union
+    {
+        char first_letter_of_person_name;
+        int robot_ID;
+        float alien_eye_light_wavelength;
+    };
+
+    // Constructors with explicit specifier
+    explicit TaggedUnion(int value);
+    explicit TaggedUnion(const char *value);
+    explicit TaggedUnion(double value);
+
+    // No destructor needed: TaggedUnion class doesn't allocate any dynamic memory or resources that need to be explicitly released.
+
+    // Setters
+    void set_person(char first_letter);
+    void set_robot(int ID);
+    void set_alien(float wavelength);
+
+    // Getters
+    char get_person() const;
+    int get_robot() const;
+    float get_alien() const;
+};
+
+#endif
